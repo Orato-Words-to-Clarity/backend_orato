7c6ed6d26306672970d43e9b4477c74ac46ebf49
M Viraj Prabhu <virajprabhup@gmail.com>
2024-12-24 17:07:36 +0530

    fix: added env for host and port

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+import os
+
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
+    host = os.getenv("HOST", "127.0.0.1")  # Default to 127.0.0.1 if HOST is not set
+    uvicorn.run("app.main:app", host=host, port=port, reload=True)
