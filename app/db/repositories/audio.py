42e2f0497e6492763a211cf7e40099cb1b0bebda
JERISHAB <jerishabmayan@gmail.com>
2025-01-07 01:43:05 +0530

    feat: Update get_audio_with_transcription to include transcriptions

--- app/db/repositories/audio.py
+++ app/db/repositories/audio.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from app.db.models.audio import Audio
 from app.db.models.transcription import Transcription
 
@@ -6,4 +6,4 @@ def get_all_audios(db: Session):
     return db.query(Audio).all()
 
 def get_audio_with_transcription(db: Session, audio_id: int):
-    return db.query(Audio).filter(Audio.audio_id == audio_id).first()
+        return db.query(Audio).options(joinedload(Audio.transcriptions)).filter(Audio.audio_id == audio_id).first()
