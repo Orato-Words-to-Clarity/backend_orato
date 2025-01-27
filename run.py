f788a41f41675a3500468f72b4222bfc1ec1220e
M Viraj Prabhu <virajprabhup@gmail.com>
2025-01-27 23:12:33 +0530

    fix: added-port-change

--- run.py
+++ run.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
 
     # Step 2: Start the FastAPI app
     port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
-    host = os.getenv("HOST", "127.0.0.1")  # Default to 127.0.0.1 if HOST is not set
+    host = os.getenv("HOST", "0.0.0.0")  # Default to 127.0.0.1 if HOST is not set
     uvicorn.run("app.main:app", host=host, port=port, reload=True)
