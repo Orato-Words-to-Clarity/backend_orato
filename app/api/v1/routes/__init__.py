34e74ea26617114c5eb35c5246e3ccf3be48bf74
M Viraj Prabhu <virajprabhup@gmail.com>
2025-01-29 16:16:54 +0530

    feat: added-interactions-router

--- app/api/v1/routes/__init__.py
+++ app/api/v1/routes/__init__.py
@@ -1,9 +1,10 @@
 
 from fastapi import APIRouter
-from app.api.v1.routes import transcription,auth,audio
+from app.api.v1.routes import interactions, transcription,auth,audio
 
 
 router = APIRouter()
 router.include_router(transcription.router, prefix="/transcription", tags=["transcription"])
 router.include_router(auth.router, prefix="/auth", tags=["auth"])
 router.include_router(audio.router, prefix="/audio", tags=["audio"])
+router.include_router(interactions.router, prefix="/interactions", tags=["interactions"]) 
